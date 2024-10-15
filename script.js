e0e7498a42ee51bbebab8cabbfdadb5c5ebaf237
Moa <svensson.moaa@gmail.com>
2024-10-15 20:50:10 +0200

    Layout klar

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Funktion för att skapa fyrverkeriet
-// Funktion för att skapa fyrverkeriet
-function createFirework(event) {
-    console.log('Fyrverkeri triggas!'); // Lägg till denna rad för att testa om funktionen körs
-    const firework = document.createElement('div');
-    firework.className = 'firework';
-
-    const rect = event.target.getBoundingClientRect();
-    firework.style.left = '50%';
-firework.style.top = '50%';
- 
-
-    document.body.appendChild(firework);
-
-    setTimeout(() => {
-        firework.remove();
-    }, 1000);
-}
-
 
 // Lägg till eventlyssnare på checkboxarna
 // Lägg till eventlyssnare på checkboxarna
